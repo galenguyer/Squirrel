e403d7a3a3696b3700a53ac936b9dbd05caf4043
Galen Guyer <galen@galenguyer.com>
2021-06-05 15:31:47 -0700

    Format code and only respond to aircraft.json

--- squirrel.py
+++ squirrel.py
@@ -1,15 +1,21 @@
 import inotify.adapters
 
+
 def main():
     i = inotify.adapters.Inotify()
 
-    i.add_watch('/run/dump1090-fa/')
+    i.add_watch("/run/dump1090-fa/")
 
     for event in i.event_gen(yield_nones=False):
         (_, type_names, path, filename) = event
 
-        print("PATH=[{}] FILENAME=[{}] EVENT_TYPES={}".format(
-              path, filename, type_names))
+        if filename == "aircraft.json":
+            print(
+                "PATH=[{}] FILENAME=[{}] EVENT_TYPES={}".format(
+                    path, filename, type_names
+                )
+            )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
