86a66e5199cd986f4b8a4a2f0870e6a075b4240a
Galen Guyer <galen@galenguyer.com>
2021-06-08 15:38:13 -0700

    Connect to mongodb and get document count

--- squirrel-cli/src/main.rs
+++ squirrel-cli/src/main.rs
@@ -1,11 +1,23 @@
 use dotenv::dotenv;
-use std::env;
+use mongodb::{
+    bson::{doc, Document},
+    options::CountOptions,
+    sync::Client,
+};
+use serde::{Deserialize, Serialize};
 
 fn main() {
     dotenv().ok();
 
-    match env::var("SQUIRREL_MONGO_URI") {
-        Ok(val) => println!("SQUIRREL_MONGO_URI: {:?}", val),
-        Err(e) => panic!("{}", e),
+    let mongo_uri = std::env::var("SQUIRREL_MONGO_URI")
+        .expect("Could not find SQUIRREL_MONGO_URI environment variabe");
+
+    let client = Client::with_uri_str(mongo_uri).expect("Couldn't connect to mongo database");
+    let database = client.database("dump1090");
+    let aircraft = database.collection::<mongodb::bson::Document>("aircraft");
+    let doc_count = aircraft.count_documents(doc! {}, CountOptions::builder().build());
+    match doc_count {
+        Ok(count) => println!("Found {} documents in aircraft database", count),
+        Err(e) => println!("Error occured getting document count: {:?}", e),
     }
 }
