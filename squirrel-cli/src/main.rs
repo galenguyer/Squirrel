d50f0edc40433255ade66257d65b7fdcc67dadb9
Galen Guyer <galen@galenguyer.com>
2021-06-07 23:44:44 -0700

    Load env file and print db uri

--- squirrel-cli/src/main.rs
+++ squirrel-cli/src/main.rs
@@ -1,3 +1,11 @@
+use dotenv::dotenv;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    dotenv().ok();
+
+    match env::var("SQUIRREL_MONGO_URI") {
+        Ok(val) => println!("SQUIRREL_MONGO_URI: {:?}", val),
+        Err(e) => panic!("{}", e),
+    }
 }
